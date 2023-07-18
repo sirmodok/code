38bbcf3291ae7b222d9cc05f8eab4c3dac3e8b93
sirmodok <sirmodok@gmail.com>
2023-07-18 12:29:17 -0500

    chromadb working

--- simplerisk/extras/ai/include/main.py
+++ simplerisk/extras/ai/include/main.py
@@ -1,30 +1,36 @@
 import os
-import pinecone
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import SystemMessage, HumanMessage
+from langchain.vectorstores import Chroma
 from langchain.sql_database import SQLDatabase
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from pprint import pprint
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
-pinecone_api_key = os.getenv("PINECONE_API_KEY")
+db_password = os.getenv("DB_PASSWORD")
 
 pinecone_location="asia-southeast1-gcp-free"
+index_name="simplerisk-test"
+
+embeddings = OpenAIEmbeddings()
+embed_model = "text-embedding-ada-002"
 
 db_user = "simplerisk"
-db_password = "n5j2zHPZ7ZYxuRg19aba"
 db_host = "localhost"
 db_name = "simplerisk"
 
 llm = ChatOpenAI(temperature=0, model="gpt-4")
 db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
-pinecone_db = pinecone.init(api_key=pinecone_api_key, environment=pinecone_location)
 
-table_list = db.get_usable_table_names()
 
-for i in table_list:
-   j =  db.run_no_throw("SELECT * FROM {}".format(i))
-   messages = [
-    SystemMessage(content="convert this to json"),
-    HumanMessage(content=j)
-   ]
-   print(llm(messages=messages))
-   
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 2048, chunk_overlap = 20)
+full_list = []
+tables = db.get_usable_table_names()
+for i in tables:
+    query = db.run_no_throw("""SELECT * FROM {}""".format(i))
+    full_list.append(query)
+
+split = text_splitter.create_documents(full_list)
+print(len(split))
+db = Chroma.from_documents(split, embeddings)
+print(db.similarity_search("How many assets are there?"))
